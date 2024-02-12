a17b26b08b1f8c93980e63047a131d56558a05a2
sameerALam00 <Sameer20.Khan@ril.com>
2024-02-12 12:10:37 +0530

    learnt data types

---
+++ Basic/Basics.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        // printing number
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
+}
